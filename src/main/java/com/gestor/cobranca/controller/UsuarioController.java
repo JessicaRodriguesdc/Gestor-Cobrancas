751fc550cf25ecce75a594c478247f3c6a55e690
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-07-08 16:01:12 -0300

    criando sistema de login

--- src/main/java/com/gestor/cobranca/controller/UsuarioController.java
+++ src/main/java/com/gestor/cobranca/controller/UsuarioController.java
@@ -1,8 +1,11 @@
 package com.gestor.cobranca.controller;
 
+import com.gestor.cobranca.model.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/cobranca")
@@ -13,18 +16,30 @@ public class UsuarioController {
     public static final String HOME_VIEW = "Home";
 
     @RequestMapping
-    public String login() {
+    public String login(Usuario usuario) {
         return LOGIN_VIEW;
     }
 
     @RequestMapping(value="/logar", method = RequestMethod.POST)
-    public String logar() {
-        return "redirect:/cobranca/titulos";
+    public String logar(Usuario usuario, HttpSession session) {
+        if(usuario.getLogin().equals("dada") && usuario.getSenha().equals("123")){
+            usuario.setNome("Jessica");
+            //Guardar sessao o objeto usuario
+            session.setAttribute("usuarioLogado",usuario);
+            return "redirect:/cobranca/titulos";
+        }else {
+            return "redirect:/cobranca";
+        }
     }
 
     @RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
-    public String cadastrar() {
+    public String cadastrar(Usuario usuario) {
         return "redirect:/cobranca/logar";
     }
 
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/cobranca";
+    }
 }
