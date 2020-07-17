e140cc17500ba40c9a526a7c1daa31cc28684bc7
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-07-17 18:58:13 -0300

    ajustando rotas na parte de configuracoes e seguranca

--- src/main/java/com/gestor/cobranca/configuracao/SecurityConfig.java
+++ src/main/java/com/gestor/cobranca/configuracao/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.gestor.cobranca.configuracao;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,22 +19,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/cobranca/cadastrar",
                         "/cobranca/logout",
                         "/cobranca/titulos",
-                        "/cobranca/titulos/novo",
-                        "/cobranca/titulos/cadastrar",
-                        "/cobranca/titulos/pesquisar",
-                        "/cobranca/titulos/{**}",
-                        "/cobranca/titulos/{**}/{**}",
-                        "/cobranca/titulos/logout")
+                        "/cobranca/titulos/*")
                 .permitAll()
+                .antMatchers(HttpMethod.PUT,"/cobranca/titulos/*/receber").denyAll()
+                .antMatchers(HttpMethod.DELETE,"/cobranca/titulos/*").denyAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/cobranca")
                 .permitAll();
+//        http.authorizeRequests().antMatchers(HttpMethod.PUT,"/cobranca/titulos/*/receber").denyAll();
+//        http.authorizeRequests().antMatchers(HttpMethod.DELETE,"/cobranca/titulos/*").denyAll();
+        http.headers().cacheControl();
+
+//        http.authorizeRequests().antMatchers(HttpMethod.GET).permitAll();
+//        http.authorizeRequests().antMatchers(HttpMethod.POST).denyAll();
+//        http.authorizeRequests().antMatchers(HttpMethod.DELETE,"/you/can/alsoSpecifyAPath").denyAll();
+//        http.authorizeRequests().antMatchers(HttpMethod.PATCH,"/path/is/Case/Insensitive").denyAll();
+//        http.authorizeRequests().antMatchers(HttpMethod.PUT,"/and/can/haveWildcards/*").denyAll();
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**","/fonts/**","/js/**","/webjars/**");
+        web.ignoring().antMatchers("/css/**","/fonts/**","/js/**");
     }
 }
